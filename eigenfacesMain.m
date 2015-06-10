804c8f2c44aea7b4b7516089bbc10473baf9b393
owi0wi <gangan35@hotmail.fr>
2015-06-10 22:30:46 +0200

    commentaire

--- eigenfacesMain.m
+++ eigenfacesMain.m
@@ -1,16 +1,17 @@
-% Face recognition by Santiago Serrano
+%------------------------------------------------------------
+% ---------  Reconnaissance faciale - Eigenfaces  -----------
+% -----------  Jihade Tika - Morgan Lienardy  ---------------
+%------------------------------------------------------------
 
 clear all
 close all
 clc
-% number of images on your training set.
-% stacksize(200000000);
-nbDossiers=10;
-nbImages=4;
+% Nombre des images dans notre base de données 
+nbDossiers=15;
+nbImages=7;
 M=40;
 
-% Chosen std and mean.
-% It can be any number that it is close to the std and mean of most of the images.
+% std and mean choisis
 um=100;
 ustd=80;
 
@@ -37,8 +38,7 @@ end
 
 
 
-% Here we change the mean and std of all images. We normalize all images.
-% This is done to reduce the error due to lighting conditions and background.
+% On change mean et std de toutes les images. On normalise toutes les images.
 for i=1:size(S,2)
 temp=double(S(:,i));
 m=mean(temp);
@@ -46,7 +46,7 @@ st=std(temp);
 S(:,i)=(temp-m)*ustd/st+um;
 end
 
-% show normalized images
+% Affichage des images normalisées
 figure(2);
 for i=1:M
 str=strcat(int2str(i),'.jpg');
@@ -57,34 +57,34 @@ subplot(ceil(sqrt(M)),ceil(sqrt(M)),i)
 imshow(img)
 drawnow;
 if i==3
-title('Normalized Training Set','fontsize',18)
+title('Le training set normalise') %,'fontsize',18
 end
 end
 
 
 % mean image
-m=mean(S,2);  % obtains the mean of each row instead of each column
-tmimg=uint8(m); % converts to unsigned 8-bit integer. Values range from 0 to 255
-img=reshape(tmimg,icol,irow); % takes the N1*N2x1 vector and creates a N1xN2 matrix
+m=mean(S,2);  % Obtient la moyenne de chaque ligne au lieu de chaque colonne
+tmimg=uint8(m); % converti en 8-bit (0<val<255)
+img=reshape(tmimg,icol,irow); % prend N1*N2 vecteurs et creer N1xN2 matrices
 img=img';
 figure(3);
 imshow(img);
-title('Mean Image','fontsize',18)
+title('Mean Image') %,'fontsize',18
 
-% Change image for manipulation
-dbx=[];    % A matrix
+% Change image pour manipulation
+dbx=[];
 for i=1:M
 temp=double(S(:,i));
 dbx=[dbx temp];
 end
 
-%Covariance matrix C=A'A, L=AA'
+% Matrice de covariance C = A'A, L = AA'
 A=dbx';
 L=A*A';
-% vv are the eigenvector for L
-% dd are the eigenvalue for both L=dbx'*dbx and C=dbx*dbx';
+% vv  : eigenvector pour L
+% dd  : eigenvalue pour L=dbx'*dbx et C=dbx*dbx';
 [vv dd]=eig(L);
-% Sort and eliminate those whose eigenvalue is zero
+% Tri et supp les val a 0
 v=[];
 d=[];
 for i=1:size(vv,2)
@@ -94,7 +94,7 @@ d=[d dd(i,i)];
 end
 end
 
-%sort, will return an ascending sequence
+% Tri croissant
 [B index]=sort(d);
 ind=zeros(size(index));
 dtemp=zeros(size(index));
@@ -109,21 +109,21 @@ d=dtemp;
 v=vtemp;
 
 
-%Normalization of eigenvectors
-for i=1:size(v,2) %access each column
+% Normalise eigenvectors
+for i=1:size(v,2)
 kk=v(:,i);
 temp=sqrt(sum(kk.^2));
 v(:,i)=v(:,i)./temp;
 end
 
-%Eigenvectors of C matrix
+% Eigenvectors pour la matrice C
 u=[];
 for i=1:size(v,2)
 temp=sqrt(d(i));
 u=[u (dbx*v(:,i))./temp];
 end
 
-%Normalization of eigenvectors
+% Normalise pour eigenvectors
 for i=1:size(u,2)
 kk=u(:,i);
 temp=sqrt(sum(kk.^2));
@@ -131,7 +131,7 @@ u(:,i)=u(:,i)./temp;
 end
 
 
-% show eigenfaces
+% montre les eigenfaces
 figure(4);
 for i=1:size(u,2)
 img=reshape(u(:,i),icol,irow);
@@ -141,12 +141,12 @@ subplot(ceil(sqrt(M)),ceil(sqrt(M)),i)
 imshow(img)
 drawnow;
 if i==3
-title('Eigenfaces','fontsize',18)
+title('Eigenfaces')%,'fontsize',18
 end
 end
 
 
-% Find the weight of each face in the training set
+% Trouve le poids de chaque image du training set
 omega = [];
 for h=1:size(dbx,2)
 WW=[];
@@ -159,15 +159,13 @@ omega = [omega WW];
 end
 
 
-% Acquire new image
-% Note: the input image must have a bmp or jpg extension.
-% It should have the same size as the ones in your training set.
-% It should be placed on your desktop
+% Pour une image a inserer 
+% Ex : 's5\8.pgm'
 InputImage = input('Please enter the name of the image and its extension \n','s');
 InputImage = imread(strcat('C:\Users\Morgan\Documents\&&Boulot\ENSSAT\IMR2\Traitements_images\Eigenfaces\',InputImage));
 figure(5)
 subplot(1,2,1)
-imshow(InputImage); colormap('gray');title('Input image','fontsize',18)
+imshow(InputImage); colormap('gray');title('Input image')%,'fontsize',18
 InImage=reshape(double(InputImage)',irow*icol,1);
 temp=InImage;
 me=mean(temp);
@@ -182,13 +180,13 @@ for i = 1:aa
 pare = dot(NormImage,u(:,i));
 p = [p; pare];
 end
-ReshapedImage = m + u(:,1:aa)*p; %m is the mean image, u is the eigenvector
+ReshapedImage = m + u(:,1:aa)*p; %m est l'image moyenne, u est l'eigenvector
 ReshapedImage = reshape(ReshapedImage,icol,irow);
 ReshapedImage = ReshapedImage';
-%show the reconstructed image.
+%Montre l'image reconstruite
 subplot(1,2,2)
 imagesc(ReshapedImage); colormap('gray');
-title('Reconstructed image','fontsize',18)
+title('Image reconstruite ')%,'fontsize',18
 
 InImWeight = [];
 for i=1:size(u,2)
@@ -201,9 +199,9 @@ ll = 1:M;
 figure(68)
 subplot(1,2,1)
 stem(ll,InImWeight)
-title('Weight of Input Face','fontsize',14)
+title('Poids de l image inseree')%,'fontsize',14
 
-% Find Euclidean distance
+% Find  distance Euclidienne
 e=[];
 for i=1:size(omega,2)
 q = omega(:,i);
@@ -215,7 +213,7 @@ end
 kk = 1:size(e,2);
 subplot(1,2,2)
 stem(kk,e)
-title('Eucledian distance of input image','fontsize',14)
+title('Distance Euclidienne de l image inseree')%,'fontsize',14
 
 MaximumValue=max(e)  % maximum eucledian distance
 MinimumValue=min(e)    % minimum eucledian distance
